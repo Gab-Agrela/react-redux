45f49df93a587b081fa40c61d1d9d9fabcb22ce0
GabrielAgrela <gabriel.agrela@hotmail.com>
2023-02-26 12:46:52 -0300

    feat: Creating files and router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Switch } from 'react-router-dom';
+
 import './App.css';
+
+import Login from './Pages/Login';
+import Wallet from './Pages/Wallet';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Switch>
+        <Route exact path="/">
+          <Login />
+        </Route>
+        <Route exact path="/wallet">
+          <Wallet />
+        </Route>
+    </Switch>
+  )
 }
 
 export default App;
